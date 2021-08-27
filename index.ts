135612c784ef257eb7e646a0623abceee15722a7
gy <372301467@qq.com>
2021-08-27 15:32:01 +0800

    删除无用代码

--- index.ts
+++ index.ts
@@ -4,14 +4,13 @@ type bread<In, Out> = (vege: vege<In, Out>, context: In) => Out
 /** 构造三明治代码 */
 export default
 function Sandwich<In = void, Out = void>(breads: bread<In, Out>[], vege: vege<In, Out>) {
-  let sandwich = vege
   let bread
   while(bread = breads.pop()) {
-    let b = bread, v = sandwich // 保留块作用域下的 bread、sandwich
-    sandwich = function(ctx) {
+    let b = bread, v = vege // 保留块作用域下的 bread、vege
+    vege = function(ctx) {
       return b(v, ctx)
     }
   }
   
-  return sandwich
+  return vege
 }
